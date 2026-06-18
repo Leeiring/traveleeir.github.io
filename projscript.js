bb997bc6e890bf8a81ec36087f154a90e457646f
Leeiring <treicelibio@gmail.com>
2026-06-18 11:18:40 +0100

    Update projscript.js

--- projscript.js
+++ projscript.js
@@ -21,20 +21,28 @@
   })
   })
 
-  const panes = document.querySelectorAll('.pane')
 
+//panes functions
+const panes = document.querySelectorAll('.pane') 
 let z = 1
 
-panes.forEach((pane) => {
-  const title = pane.querySelector('.title')
-  const corner = pane.querySelector('.corner')
+panes.forEach((pane) => { //
+  const title = pane.querySelector('.title') //blue title section
+  const corner = pane.querySelector('.corner') //bottom right corner
 
-  pane.addEventListener('mousedown', () => {
+  pane.addEventListener('mousedown', () => { // move laast clicked pane to the front
     z = z + 1
     pane.style.zIndex = z
   })
 
-  title.addEventListener('mousedown', (event) => {
+  pane.addEventListener('touchstart', () => { // move last tapped pane to the front
+    z = z + 1
+    pane.style.zIndex = z
+  })
+
+ 
+
+  title.addEventListener('mousedown', (event) => { //dragging event desktop
     pane.classList.add('is-dragging')
 
     let l = pane.offsetLeft
@@ -61,7 +69,40 @@ panes.forEach((pane) => {
     document.addEventListener('mouseup', mouseup)
   })
 
-  corner.addEventListener('mousedown', (event) => {
+
+  title.addEventListener('touchstart', (event) => { //dragging event mobile
+    pane.classList.add('is-dragging')
+
+    let l = pane.offsetLeft
+    let t = pane.offsetTop
+
+    let startX = event.pageX
+    let startY = event.pageY
+
+    const drag = (event) => {
+      event.preventDefault()
+
+      pane.style.left = l + (event.pageX - startX) + 'px'
+      pane.style.top = t + (event.pageY - startY) + 'px'
+    }
+
+    const touchend = () => {
+      pane.classList.remove('is-dragging')
+
+      document.removeEventListener('touchmove', drag)
+      document.removeEventListener('touchend', touchend)
+    }
+
+    document.addEventListener('touchmove', drag)
+    document.addEventListener('touchend', touchend)
+  })
+
+
+
+
+
+  //extending and shinking pane
+  corner.addEventListener('mousedown', (event) => { //drag = edit pane on mouse down end event on mouse up 
     let w = pane.clientWidth 
     let h = pane.clientHeight
 
@@ -83,6 +124,33 @@ panes.forEach((pane) => {
     document.addEventListener('mousemove', drag)
     document.addEventListener('mouseup', mouseup)
   })
+
+
+  //extending and shinking pane mobile
+  corner.addEventListener('touchstart', (event) => { //drag = edit pane on mouse down end event on mouse up 
+    let w = pane.clientWidth 
+    let h = pane.clientHeight
+
+    let startX = event.pageX
+    let startY = event.pageY
+    
+
+    const drag = (event) => {
+      event.preventDefault()
+
+      pane.style.width = w + (event.pageX - startX) + 'px'
+      pane.style.height = h + (event.pageY - startY) + 'px'
+    }
+
+    const touchend = () => {
+      document.removeEventListener('touchmove', drag)
+      document.removeEventListener('touchend', touchend)
+    }
+
+    document.addEventListener('touchmove', drag)
+    document.addEventListener('touchend', touchend)
+  })
+
 })
 
 
